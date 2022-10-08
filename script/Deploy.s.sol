7faac0c3b1bbb7f128a89696f271c2a4d46c7831
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-10-08 18:25:05 +0200

    fixed deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,17 +3,15 @@ pragma solidity >=0.8;
 
 import "forge-std/Script.sol";
 
-import { PriceFeed } from "../contracts/PriceFeed.sol";
-import { CREATE3Factory } from "create3-factory/Create3Factory.sol";
+import {PriceFeed} from "../contracts/PriceFeed.sol";
+import {CREATE3Factory} from "create3-factory/CREATE3Factory.sol";
 
 contract DeployScript is Script {
   function run() public returns (PriceFeed priceFeed) {
     address uniswapV3Factory = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
-    CREATE3Factory create3Factory = CREATE3Factory(
-      0x9fBB3DF7C40Da2e5A0dE984fFE2CCB7C47cd0ABf
-    );
+    CREATE3Factory create3Factory = CREATE3Factory(0x9fBB3DF7C40Da2e5A0dE984fFE2CCB7C47cd0ABf);
 
-    bytes32 salt = keccak256(vm.envBytes("SALT"));
+    bytes32 salt = keccak256(bytes(vm.envString("SALT")));
     uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
     vm.startBroadcast(deployerPrivateKey);
