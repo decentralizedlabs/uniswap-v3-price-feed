6f4f0fe5a21d541184298e7a71fc326fa568e4c1
Dom-Mac <domenicochad@gmail.com>
2022-10-11 14:59:38 +0200

    on push

--- script/mergeToEarn.js
+++ script/mergeToEarn.js
@@ -7,6 +7,7 @@ try {
   console.log(`Hello ${nameToGreet}!`);
   const time = new Date().toTimeString();
   core.setOutput("time", time);
+  core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
